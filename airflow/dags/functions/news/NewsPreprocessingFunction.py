8222079b9eb2ae996f95e6650aa3a0d05bdd95fc
sehunkim <sehun5515@naver.com>
2024-09-25 17:43:34 +0900

    feat: implements call fast api module

--- airflow/dags/functions/news/NewsPreprocessingFunction.py
+++ airflow/dags/functions/news/NewsPreprocessingFunction.py
@@ -1,16 +1,56 @@
+import requests
+
+domain: str = "70.12.247.100:8080"
 
 
 def embedding_processing(**context):
-    pass
+    collected_data: list[dict] = context['task_instance'].xcom_pull(task_ids='collecting_data')
+
+    # request to fast api gpu server
+    request_url: str = f'http://{domain}/embedding'
+    response = requests.post(url=request_url, json=collected_data)
+
+    if response.status_code != 200:
+        raise Exception(response.status_code, response.json())
+
+    result = response.json()['data']
+    print(type(result))
+
+    return result
+
 
 def ner_processing(**context):
-    pass
+    embedding_data: list = context['task_instance'].xcom_pull(task_ids='embedding_processing')
+
+    # request to fast api gpu server
+    request_url: str = f'http://{domain}/ner'
+    response = requests.post(url=request_url, json=embedding_data)
+
+    if response.status_code != 200:
+        raise Exception(response.status_code, response.json())
+
+    return response.json()['data']
+
 
 def summarization_processing(**context):
-    pass
+    ner_data: list = context['task_instance'].xcom_pull(task_ids='ner_processing')
 
-def store_to_elastic_search(**context):
-    pass
+    # request to fast api gpu server
+    request_url: str = f'http://{domain}/summarize'
+    response = requests.post(url=request_url, json=ner_data)
 
-def store_to_hadoop(**context):
+    if response.status_code != 200:
+        raise Exception(response.status_code, response.json())
+
+    return response.json()['data']
+
+
+def store_to_elastic_search(**context) -> None:
+    summarized_data: list = context['task_instance'].xcom_pull(task_ids='summarization_processing')
+
+    for data in summarized_data:
+        print(data)
+
+
+def store_to_hadoop(**context) -> None:
     pass
