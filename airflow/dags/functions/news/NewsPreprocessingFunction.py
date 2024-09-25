71e6d7a8395853be3ac6289717939ebe53cd75e3
sehunkim <sehun5515@naver.com>
2024-09-25 13:44:24 +0900

    feat: add News Preprocessing Node

---
+++ airflow/dags/functions/news/NewsPreprocessingFunction.py
@@ -0,0 +1,16 @@
+
+
+def embedding_processing(**context):
+    pass
+
+def ner_processing(**context):
+    pass
+
+def summarization_processing(**context):
+    pass
+
+def store_to_elastic_search(**context):
+    pass
+
+def store_to_hadoop(**context):
+    pass
