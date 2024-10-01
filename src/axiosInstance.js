b963bffc313dfb267106b952e89af1c50d6372da
박정영 <bjy123bjy@gmail.com>
2024-10-01 17:48:28 +0900

    fix: 디렉토리 이동, fe 브랜치가 저희 master와 너무 달라서 디렉토리 정리랑 싱크로좀 맞췃습니다.

--- src/axiosInstance.js
+++
@@ -1,8 +0,0 @@
-// src/axiosInstance.js
-import axios from "axios";
-
-const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL, // 기본 URL 설정
-});
-
-export default axiosInstance;
