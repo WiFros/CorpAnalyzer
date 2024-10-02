f219144b0401cc65eaafabe7fdbc3fa00510bc4a
홍수연 <sdfg1001@naver.com>
2024-10-02 14:18:50 +0900

    Update front/public/vite.svg, front/src/assets/react.svg, front/src/styles/globals.css, front/src/App.css, front/src/App.jsx, front/src/main.jsx, front/src/router.jsx, front/.gitignore, front/README.md, front/eslint.config.js, front/index.html, front/postcss.config.js, front/vite.config.js Deleted front/public/favicon.ico, front/public/index.html, front/public/robots.txt, front/src/components/Header.js, front/src/components/Layout.js, front/src/components/TextSphere.js, front/src/pages/CompanyFinancialPage.js, front/src/pages/CompanyInfoPage.js, front/src/pages/CompanyKeywordPage.js, front/src/pages/CompanyNewsPage.js, front/src/pages/CompanyPage.js, front/src/pages/MainSearchPage.js, front/src/pages/SearchResultPage.js, front/src/styles/TextShpere.css, front/src/App.js, front/src/App.test.js, front/src/axiosInstance.js, front/src/index.css, front/src/index.js, front/src/next.config.js, front/src/reportWebVitals.js, front/src/router.js, front/src/setupTests.js, front/package-lock.json, front/package.json, front/tailwind.config.js

--- front/src/pages/CompanyFinancialPage.js
+++
@@ -1,6 +0,0 @@
-import React, { useEffect, useState, useCallback } from "react";
-import { useLocation } from "react-router-dom";
-
-const CompanyFinancialPage = () => {};
-
-export default CompanyFinancialPage;
