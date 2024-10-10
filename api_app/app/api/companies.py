4d6328e35c4181ecf78b9b4617d308febdc648a8
jhyun9505 <jhyun9505@gmail.com>
2024-10-10 10:38:59 +0900

    feat: hotkeyword with news feature added

--- api_app/app/api/companies.py
+++ api_app/app/api/companies.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter, Query, Depends, HTTPException
 from app.services.company_search import CompanySearchService
 from app.services.hotKeyword_search import hotKeywordService
 from app.models.company import CompanyList
-from app.models.hotkeyword import KeywordList
+from app.models.hotkeyword import KeywordList,KeywordListWithNews, KeywordNews
 from app.database import get_database
 
 companies_router = APIRouter()
@@ -31,5 +31,31 @@ async def company_hotkeyword(
     try:
         results = await hotkeyword_service.fetch_hotkeyword()
         return KeywordList(**results)
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+@companies_router.get("/hotkeyword_with_news", response_model=KeywordListWithNews)
+async def company_hotkeyword(
+    corp_name: str ,
+):
+    hotkeyword_service = hotKeywordService(corp_name)
+    try:
+        results = await hotkeyword_service.fetch_hotkeyword_with_news()
+        keywords = {}
+        for word, news_list in results.items():
+            keyword = []
+            for news in news_list:
+                keyword.append(KeywordNews(
+                    title=news['title'],
+                    pubDate= news['pubDate'],
+                    link=news['link']
+                ))
+            keywords[word] = keyword
+        res = KeywordListWithNews(
+            corp_name= corp_name,
+            keywords = keywords
+        )
+        return res
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
